c8ed22eab5b3176b5e7b48d77a875f4c5d595a06
Abcdef2393 <zac.noche@gmail.com>
2026-08-17 16:06:05 -0400

    Add files via upload

---
+++ wikipediatest/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+// extra
+// app.use(express.placeholder());
+
+// replace the path with anything, remove the path, because it can affect the link, make sure it is like: ("", (req, res) // => {, to remove the path
+
+async function searchWikipedia(query) {
+    const response = await fetch(
+        `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(query)}`
+    );
+
+    const data = await response.json();
+
+    return data.extract;
+}
+
+searchWikipedia("India").then(result => {
+    console.log(result);
+});
+
+/*
+app.get("", async (req, res) => {
+
+    // use this as the input (what is being sent)
+
+    const input = req.query.input;
+    const result = await searchWikipedia(input);
+    res.send(result);
+});
+*/
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running");
+});
