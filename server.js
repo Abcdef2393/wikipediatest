4ebe4cc49db713ed50aa65e8da141b9467214ba5
Abcdef2393 <zac.noche@gmail.com>
2026-08-17 17:54:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ searchWikipedia("India").then(result => {
 */
 
 app.post("/:id", async (req, res) => {
+    console.log("POST recieved");
     const id = req.params.id;
     console.log(req.body);
     const input = req.body.input;
