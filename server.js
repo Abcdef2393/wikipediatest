3d7878dd85ab62fb4fde9f0a9b911053e01c7b5c
Abcdef2393 <zac.noche@gmail.com>
2026-08-17 17:58:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const queries = {};
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log("REQUEST:", req.method, req.url);
+    next();
+});
 console.log("success");
 // extra
 // app.use(express.placeholder());
